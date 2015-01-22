607bac3df3c1ade8eca6c9d165930ae1c10e87cb
Aaron Hill <aahill50@gmail.com>
2015-01-22 11:03:25 -0500

    Slight DRY refactor

--- tests/autocomplete.js
+++ tests/autocomplete.js
@@ -1,12 +1,13 @@
 module.exports = {
     "Autocomplete test" : function (browser) {
+        var searchBar = 'input[name=q]';
         var searchList = 'ul#ui-id-1';
         var searchTerm = 'gold';
 
         browser
             .url("http://1stdibs.com")
             .waitForElementVisible('body', 1000)
-            .setValue('input[name=q]', 'gold')
+            .setValue(searchBar, searchTerm)
             .waitForElementVisible(searchList, 1000)
             .assert.containsText(searchList, 'POPULAR SEARCHES')
             .assert.containsText(searchList, 'DEALERS')
