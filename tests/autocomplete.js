dbf95660ba8822e328468809050e212713b2fb72
Aaron Hill <aahill50@gmail.com>
2015-01-23 12:11:54 -0500

    Added chai. Refactored auto complete test to use chai assertions.

--- tests/autocomplete.js
+++ tests/autocomplete.js
@@ -1,17 +1,35 @@
-module.exports = {
-    "Autocomplete test 1" : function (browser) {
-        var searchBar = 'input[name=q]';
-        var searchList = 'ul#ui-id-1';
-        var searchTerm = 'gold';
+"use strict";
 
-        browser
-            .url("http://1stdibs.com")
-            .waitForElementVisible('body', 1000)
-            .setValue(searchBar, searchTerm)
-            .waitForElementVisible(searchList, 1000)
-            .assert.containsText(searchList, 'POPULAR SEARCHES')
-            .assert.containsText(searchList, 'DEALERS')
-            .assert.containsText(searchList, searchTerm)
+var chai = require('chai'),
+    expect = chai.expect,
+    should = chai.should();
+
+    module.exports = {
+        before : function(browser) {
+            browser.searchBar = 'input[name=q]';
+            browser.searchList = 'ul#ui-id-1';
+            browser.searchTerm = 'gold';
+        },
+
+        "Auto Complete - Enter Search Term" : function (browser) {
+            browser
+                .url("http://1stdibs.com")
+                .waitForElementVisible('body', 1000)
+                .setValue(browser.searchBar, browser.searchTerm)
+                .waitForElementVisible(browser.searchList, 1000, function () {
+                    browser.searchTerm.should.equal(browser.searchTerm);
+                })
+        },
+
+        "Auto Complete - Validate Search Results" : function (browser) {
+            browser
+                .getText(browser.searchList, function (result) {
+                    var searchResults = result.value;
+                    //searchResults.should.contain('123');
+                    searchResults.should.contain('POPULAR SEARCHES');
+                    searchResults.should.contain('DEALERS');
+                    searchResults.should.contain(browser.searchTerm);
+                })
             .end();
     }
 };
